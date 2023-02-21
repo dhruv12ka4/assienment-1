cf785741505ace88126566e68be811b6b8bbb2a3
dhruv12ka4 <125910898+dhruv12ka4@users.noreply.github.com>
2023-02-21 13:24:54 +0530

    Add files via upload

---
+++ super---.python.py
@@ -0,0 +1,59 @@
+#if 'a' in 'apple':
+#    print('1')
+#    print('2')
+#    print('3')
+#else:
+#    print('a is present')
+#
+#number = 5
+#message = "Even" if number % 2 == 0 else "odd"
+#print(message)
+#
+# Check character is vovel or not
+
+#char = input('Enter a character : ')
+#vovel = "aeiou"
+#
+#if char in vovel:
+#    print('yes it is vovel')
+#else:
+#    print('yes it is not vovel')
+
+
+# Fibonacci sequence seris
+
+num1 = 0
+num2 = 1
+a = input("print fibonacci series:")
+while num1 < 50:
+    print(num1)
+    num3 = num1 + num2
+    num1 = num2
+    num2 = num3
+    
+
+
+#list = [1,2,3,4,5,6,7,8,9]
+#even = 0
+#odd = 0
+#for i in list:
+#    if(i%2==0):
+#        even=even+1
+#    else:
+#        odd=odd+1
+#
+#print("Even number in list",even)
+#print("Odd number in list",odd)
+#
+#
+#a = input("Enter a string:")
+#for word in a.split():
+#    print(word[::-1],end=" ")
+
+
+
+
+
+
+
+   
